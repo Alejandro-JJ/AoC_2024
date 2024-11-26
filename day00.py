491ef480a2ea91ccd8b04145fb85f8b24e91f3dd
AlejandroDSKTP <alex.pf314@gmail.com>
2024-11-26 23:52:09 +0100

    Preparation tools

---
+++ day00.py
@@ -0,0 +1,13 @@
+path = 'C:\\Users\\Alejandro\\Desktop\\AoC\\AoC_2024\\input_00.txt'
+with open(path, 'r') as f:
+    cosa = [[s for s in l.strip('\n')] for l in f.readlines()]
+print(cosa)
+
+# Custom dict
+my_dic = {'.':0, '#':1}
+from tools import  Text2Array
+import matplotlib.pyplot as plt
+A = Text2Array(cosa, my_dic)
+print(A)
+plt.imshow(A)
+plt.show()
