547d5392800137feafd02a9106af78353a43b7e3
AlejandroDSKTP <alex.pf314@gmail.com>
2024-12-01 06:29:47 +0100

    Day 01

---
+++ day01.py
@@ -0,0 +1,21 @@
+#
+path = r"C:\Users\Alejandro\Desktop\AoC\AoC_2024\input_01.txt"
+with open(path, "r") as f:
+    data = [l.split() for l in f.readlines()]
+left = sorted([int(x[0]) for x in data])
+right = sorted([int(x[1]) for x in data])
+
+dist = 0
+for n in range(0, len(left)):
+    dist+=abs(left[n]-right[n])
+print(dist)
+
+# Part 2, without sort
+left = [int(x[0]) for x in data]
+right = [int(x[1]) for x in data]
+sim = 0
+for n in left:
+    reps = sum([s==n for s in right])
+    sim += n*reps
+print(sim)
+
