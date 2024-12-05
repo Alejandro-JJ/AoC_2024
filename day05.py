69cd82cb71794a18743c4a8c0cceb57c876bb748
AlejandroDSKTP <alex.pf314@gmail.com>
2024-12-05 09:22:48 +0100

    Day 5

--- day05.py
+++ day05.py
@@ -1,10 +1,70 @@
-'''
-Day 5:
-'''
-import re
-import numpy as np
-import matplotlib.pyplot as plt
+from tqdm import tqdm
+from functools import cmp_to_key
 
-path = r"C:\Users\Alejandro\Desktop\AoC\AoC_2024\input_05.txt"
+path = r"./5.txt"
 with open(path, "r") as f:
-    data =  [[s for s in line.strip('\n')] for line in f.readlines() ]
+    data =  [line.strip('\n') for line in f.readlines() ] 
+
+# get rules and manuals
+rules = []
+manuals = []
+for line in data:
+    if '|' in line:
+        left, right = list(map(int, line.split('|')))
+        rules.append([left, right])
+    elif ',' in line:
+        manual = list(map(int, line.split(',')))
+        manuals.append(manual)
+
+# define comparing function
+def compare(manual, left, right):
+    if manual.index(left)<manual.index(right):
+        return True
+    else: 
+        return False    
+
+# for part 2, custom sorting funct
+def sort_by_rules(left, right):
+    # right order
+    if [left, right] in rules:
+        return -1
+    # wrong order
+    elif [right, left] in rules:
+        return 1
+    else:
+        return 0
+
+    
+# Go through each manual and use the valid rules
+valid = []
+invalid = [] # for part 2
+
+for manual in tqdm(manuals):
+    # here we save bool for manual
+    passed = [] 
+    
+    for rule in rules:
+        left, right = rule
+        # If number of rule are not present, ignore
+        if left in manual and right in manual:
+            passed.append(compare(manual, left, right)) 
+        else:
+            pass     
+    # After comparing all rules, 
+    # manual is valid is all rules were passed
+    if all(passed):
+        valid.append(manual)
+    else:
+        invalid.append(manual)
+        
+sol = 0
+for manual in valid:
+    sol += manual[len(manual)//2]
+print(f'Solution part 1: {sol}\n')
+
+sol2 = 0
+for manual in invalid:
+    newmanual = sorted(manual, key=cmp_to_key(sort_by_rules))
+    print(f'Conversion: {manual} -> {newmanual}')
+    sol2 += newmanual[len(newmanual)//2]
+print(f'Solution part 2: {sol2}\n')
--- tools.py
+++ tools.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
+import requests 
+import os
 
 def Text2Array(lista, dic):
     '''
@@ -17,4 +20,32 @@ def Text2Array(lista, dic):
                 A[r,c] = dic[lista[r][c]]
     return A
 
-
+def GetInput(year= 2024, day=4):
+    '''
+    Scraping function to get input from webpage
+    In order to no overload server, we only 
+    scrape if a local file is not present
+    '''
+    filepath = f'./input_{str(day).zfill(2)}.txt'
+    if os.path.exists(filepath):
+        print('Input file exists, did not fetch. Reading...')
+        
+    else:
+        # Else, fetch and save, to later read
+        # Session cookie is not tracked in Git
+        print('Fetching input from AoC')
+        cookiepath = r'c:\Users\Alejandro\Desktop\AoC\cookie_AoC.txt'
+        with open(cookiepath, 'r') as f:
+            session_cookie = f.readline()
+        cookies = {"session": session_cookie}
+        url = f'https://adventofcode.com/{year}/day/{day}/input'
+        web = requests.get(url, cookies=cookies)
+        text = web.text
+        # save
+        with open(filepath, 'w') as f:
+            f.write(text)
+    
+    # In either case, read the saved data
+    with open(filepath, 'r') as f:
+        data = f.read()
+    return data
