daf8aaa76aadb9dfa4ca1d724032d06d196fcabf
AlejandroDSKTP <alex.pf314@gmail.com>
2024-12-06 10:39:54 +0100

    Day 6, now with part 2

--- day06.py
+++ day06.py
@@ -2,6 +2,7 @@ from tools import  Text2Array
 import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
+from time import time
 
 path = r"./6.txt"
 with open(path, "r") as f:
@@ -51,25 +52,66 @@ and run the simulation again. In this case however, we need to
 check for loops. You can do that saving a state vector (pos, dir)
 If you ever come to the same position with the same direction, 
 you are stuck in a loop 
+
+OPTIMIZED ROUTINE: only check obstacle positions 
+which would interfere with the original path!!
+
+Optimization idea: don't move step by step, but directly 
+teleport to the next encountered obstacle in the path!
+
 '''
+# Part 2
+obstaclepos = list(zip(np.where(mapa==0)[0], np.where(mapa==0)[1]))
+print(f'There are {len(obstaclepos)} potential obstacles')
+validobstacles = [i for i in obstaclepos if i in visited]
+print(f'There are {len(validobstacles)} valid obstacles')
 
+def EvaluateMap(mapa):
+    '''
+    Takes a map and returns:
+        'Loop' if stuck in a loop
+        'Open' if the guard escapes
+    
+    We keep track of an "state" tuple
+    which contains (pos,dir)
+    If this state has been visited before, is a loop
+    '''
+    currpos = (np.where(mapa==3)[0][0], np.where(mapa==3)[1][0])
+    currdir = (-1,0) # starts upwards
+    visited_states = [(currpos, currdir)]
 
-""" # Video generation
-savepath = "C:\\Users\\Alejandro\\Desktop\\AoC\\day6video\\"
-fig, ax = plt.subplots(1,1,figsize=(5,5))
-ax.imshow(mapa, cmap='Set3')
-ax.set_axis_off()
-plt.tight_layout()
-plt.savefig(savepath+'f_00000.png')
+    while True:
+        # Calculate next potential position
+        newpos = (currpos[0]+currdir[0], currpos[1]+currdir[1])
+        
+        # If we exit the map, break loop
+        if not 0<=newpos[0]<H or not 0<=newpos[1]<W:
+            return 'Open'
+        
+        # if we have been in this state before, break the loop
+        elif (newpos, currdir) in visited_states:
+            return 'Loop'
+        
+        # If the position is occupied, rotate 90 and restart loop
+        elif mapa[newpos[0], newpos[1]]==1:
+            currdir = rot90(currdir)
+            continue
+        
+        # If none of the above conditions are fulfilled, we can take a step
+        else:
+            currpos = newpos
+            #print(f'Moved to: {currpos}')
+            # and save this state
+            visited_states.append((currpos, currdir))
 
-f = 1
-for v in tqdm(visited):
-    # Overwrite map
-    mapa[v[0], v[1]]=3
-    ax.cla()
-    ax.imshow(mapa, cmap='Set3')
-    ax.set_axis_off()
-    plt.tight_layout()
-    plt.savefig(savepath+'f_' + str(f).zfill(5) + '.png')
-    f += 1
-         """
+start = time()
+LOOPS = 0
+for obs in tqdm(validobstacles):
+    #print(f'Testing obstacle in pos {obs[0]}, {obs[1]}')
+    newmapa = mapa.copy()
+    newmapa[obs[0],obs[1]] = 1
+    #print(EvaluateMap(newmapa))
+    if EvaluateMap(newmapa)=='Loop':
+        LOOPS+=1
+print(f'Loops found: {LOOPS}')
+print(f'Time required: {(time()-start)/60} minutes')
